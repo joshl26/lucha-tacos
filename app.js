9a1b93e3b3752d8458adc860a8871f99b3a9677a
Joshua Lehman <joshlehman.dev@gmail.com>
2024-06-04 21:07:37 -0400

    updates

---
+++ app.js
@@ -0,0 +1,19 @@
+// cart
+let cart = [];
+
+// buttons
+let buttonsDOM = [];
+
+// getting the menu items
+class MenuItems {}
+
+// display menu items
+class UI {}
+
+// local storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const menuItems = new MenuItems();
+});
